ab8825f938f107b299da0c0edaa6a4792ac87afb
iBragimoff <ibragimoff.ata@gmail.com>
2020-12-17 12:23:06 +0300

    added section-four, slider wrote on slick

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/section-four-slider-prev.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="images/icons/section-four-slider-next.png"></button>'
+    });
+});
