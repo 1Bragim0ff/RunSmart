c0d5293732bc9dfbce8375271f5144a663ef33cd
iBragimoff <ibragimoff.ata@gmail.com>
2020-12-18 17:56:28 +0300

    added section-five

--- js/script.js
+++ js/script.js
@@ -6,4 +6,25 @@ $(document).ready(function(){
       prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/section-four-slider-prev.png"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="images/icons/section-four-slider-next.png"></button>'
     });
+
+    $('ul.tabs').on('click', 'li:not(.tab_active)', function() {
+        $(this)
+          .addClass('tab_active').siblings().removeClass('tab_active')
+          .closest('div.container').find('div.cards').removeClass('cards_active').eq($(this).index()).addClass('cards_active');
+    });
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.card__inner').eq(i).toggleClass('card__inner_active')
+        })
+        })
+    };
+
+    toggleSlide('.card__more');
+    toggleSlide('.card__back');
 });
+
+
