601582fcff0e7db655c371777573c3449e5418f2
iBragimoff <ibragimoff.ata@gmail.com>
2020-12-25 00:35:04 +0300

    added sending a letter to the mail and a link to scroll the page up

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-  $('.slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/section-four-slider-prev.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="images/icons/section-four-slider-next.png"></button>'
+$(document).ready(function() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/section-four-slider-prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/section-four-slider-next.png"></button>'
     });
 
     $('ul.tabs').on('click', 'li:not(.tab_active)', function() {
         $(this)
-          .addClass('tab_active').siblings().removeClass('tab_active')
-          .closest('div.container').find('div.cards').removeClass('cards_active').eq($(this).index()).addClass('cards_active');
+            .addClass('tab_active').siblings().removeClass('tab_active')
+            .closest('div.container').find('div.cards').removeClass('cards_active').eq($(this).index()).addClass('cards_active');
     });
 
 
     function toggleSlide(item) {
         $(item).each(function(i) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $('.card__inner').eq(i).toggleClass('card__inner_active')
-        })
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.card__inner').eq(i).toggleClass('card__inner_active')
+            })
         })
     };
 
@@ -53,6 +53,7 @@ $(document).ready(function(){
                     email: true
                 }
             },
+
             messages: {
                 name: "Пожалуйста, введите своё имя",
                 phone: "Пожалуйста, введите свой номер телефона",
@@ -61,6 +62,7 @@ $(document).ready(function(){
                     email: "Неправильно введен адрес почты"
                 }
             },
+
             errorPlacement: function(error, element) {
                 error.insertBefore(element);
             }
@@ -71,4 +73,36 @@ $(document).ready(function(){
     validateForms('#consultation form');
     validateForms('#consultation-form');
     validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #successfully').fadeIn();
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageUp').fadeIn();
+        } else {
+            $('.pageUp').fadeOut();
+        }
+    });
+
+    $('a[href^="#"]').click(function() {
+        const _href = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
